0037a2a4651d1fe55ac4e363a3dbf19b837c29d9
Gowzer7 <105177533+Gowzer7@users.noreply.github.com>
2025-04-11 03:22:44 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        jaysBlue: '#134a8e',
+        jaysRed: '#e8291c',
+        jaysDark: '#0d1117',
+        cardDark: '#161b22',
+        textLight: '#f0f6fc',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
