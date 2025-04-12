fc12756628577309a6f7c2f2af04ffc24eaee9d3
McGowan Weddig <mcgowan@McGowans-Air.lan>
2025-04-11 21:31:25 -0400

    lil update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }) {
